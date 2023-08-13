ebff9b3ba1ccf4f7460e36bf91e0dbedbf674307
tomplt <tompaulat@gmail.com>
2023-08-13 19:27:49 +0200

    2 | adding enemy sprites and moving pictures to player structs

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/TomPlt/test_epitengine/player"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font/basicfont"
 )
@@ -20,19 +19,14 @@ const (
 )
 
 type Game struct {
-	players     []player.Player
-	gopherImage *ebiten.Image
+	players []player.Player
+	// gopherImage *ebiten.Image
 }
 
-func (g *Game) init() {
-	// Load the image for gopher during initialization.
-	var err error
-	originalImage, _, err := ebitenutil.NewImageFromFile("images/gopher.png")
-	g.gopherImage = resizeImage(originalImage, 70, 50)
-	if err != nil {
-		log.Fatalf("Failed to load the gopher image: %v", err)
-	}
-}
+// func (g *Game) init() {
+// 	// Load the image for gopher during initialization.
+
+// }
 
 func (g *Game) Update() error {
 	// Handle arrow keY  inputs
@@ -48,13 +42,13 @@ func (g *Game) Update() error {
 		if ebiten.IsKeyPressed(ebiten.KeyUp) && g.players[i].Y > 0 {
 			g.players[i].MoveUp()
 		}
-		if ebiten.IsKeyPressed(ebiten.KeyDown) && g.players[i].Y < float64(screenHeight-g.gopherImage.Bounds().Dy()) {
+		if ebiten.IsKeyPressed(ebiten.KeyDown) && g.players[i].Y < float64(screenHeight-g.players[i].PlayerImage.Bounds().Dy()) {
 			g.players[i].MoveDown()
 		}
 		if ebiten.IsKeyPressed(ebiten.KeyLeft) && g.players[i].X > 0 {
 			g.players[i].MoveLeft()
 		}
-		if ebiten.IsKeyPressed(ebiten.KeyRight) && g.players[i].X < float64(screenWidth-g.gopherImage.Bounds().Dx()) {
+		if ebiten.IsKeyPressed(ebiten.KeyRight) && g.players[i].X < float64(screenWidth-g.players[i].PlayerImage.Bounds().Dx()) {
 			g.players[i].MoveRight()
 		}
 	}
@@ -73,7 +67,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	for _, p := range g.players {
 		op := &ebiten.DrawImageOptions{}
 		op.GeoM.Translate(p.X, p.Y)
-		screen.DrawImage(g.gopherImage, op)
+		screen.DrawImage(p.PlayerImage, op)
 	}
 	for counter, i := range g.players {
 		positionText := fmt.Sprintf("X: %v, Y: %v", i.X, i.Y)
@@ -85,31 +79,13 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-func resizeImage(src *ebiten.Image, width, height int) *ebiten.Image {
-	// Create a new blank image with the desired width and height
-	target := ebiten.NewImage(width, height)
-
-	// Define the scale factors
-	sx := float64(width) / float64(src.Bounds().Dx())
-	sy := float64(height) / float64(src.Bounds().Dy())
-
-	// Set up the image options
-	opts := &ebiten.DrawImageOptions{}
-	opts.GeoM.Scale(sx, sy)
-
-	// Draw the original image onto the target image with the scaling transformation
-	target.DrawImage(src, opts)
-
-	return target
-}
-
 func main() {
 	game := &Game{}
-	game.init()
+	// game.init()
 
 	game.players = []player.Player{
-		player.NewPlayer("Player", float64(screenWidth)/2, float64(screenHeight)/2, player.PlayerType),
-		player.NewPlayer("Merchant", float64(screenWidth)/2, float64(screenHeight)/6, player.NPCType), // I replaced NPCType with OtherType because in the previous example, I did not introduce NPCType.
+		player.NewPlayer("Player", float64(screenWidth)/2, float64(screenHeight)/2, player.PlayerType, "images/gopher.png"),
+		player.NewPlayer("Merchant", float64(screenWidth)/2, float64(screenHeight)/6, player.NPCType, "images/croco.png"),
 	}
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
--- player/player.go
+++ player/player.go
@@ -1,49 +1,78 @@
 package player
 
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
 type Charactertype string
 
 const (
 	Speed                    = 10
 	PlayerType Charactertype = "player"
-	EnemyType  Charactertype = "enemy"
 	NPCType    Charactertype = "npc"
 )
 
 type Player struct {
-	Name     string
-	X, Y     float64
-	charType Charactertype
+	Name        string
+	X, Y        float64
+	CharType    Charactertype
+	PlayerImage *ebiten.Image
 }
 
-func NewPlayer(name string, x float64, y float64, charType Charactertype) Player {
+func resizeImage(src *ebiten.Image, width, height int) *ebiten.Image {
+	target := ebiten.NewImage(width, height)
+	sx := float64(width) / float64(src.Bounds().Dx())
+	sy := float64(height) / float64(src.Bounds().Dy())
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Scale(sx, sy)
+	target.DrawImage(src, opts)
+	return target
+}
+
+func NewPlayer(name string, x float64, y float64, CharType Charactertype, image string) Player {
+	originalImage, _, err := ebitenutil.NewImageFromFile(image)
+	if err != nil {
+		log.Fatalf("Failed to load the image: %v", err)
+	}
+
+	if originalImage == nil {
+		log.Fatal("The original image is nil.")
+	}
+
+	imageOut := resizeImage(originalImage, 70, 50)
+
 	return Player{
-		Name:     name,
-		X:        x,
-		Y:        y,
-		charType: charType,
+		Name:        name,
+		X:           x,
+		Y:           y,
+		CharType:    CharType,
+		PlayerImage: imageOut,
 	}
 }
 
 func (p *Player) MoveUp() {
-	if p.charType == PlayerType {
+	if p.CharType == PlayerType {
 		p.Y -= Speed
 	}
 }
 
 func (p *Player) MoveDown() {
-	if p.charType == PlayerType {
+	if p.CharType == PlayerType {
 		p.Y += Speed
 	}
 }
 
 func (p *Player) MoveLeft() {
-	if p.charType == PlayerType {
+	if p.CharType == PlayerType {
 		p.X -= Speed
 	}
 }
 
 func (p *Player) MoveRight() {
-	if p.charType == PlayerType {
+	if p.CharType == PlayerType {
 		p.X += Speed
 	}
 }
@@ -53,8 +82,3 @@ func (p *Player) ResetPosition(screenWidth, screenHeight int) {
 	p.X = halfWidth
 	p.Y = halfHeight
 }
-
-// func (p *Player) ResetPosition(screenWidth, screenHeight, rectSize int) {
-// 	p.X = float64(screenWidth/2 - rectSize/2)
-// 	p.Y = float64(screenHeight/2 - rectSize/2)
-// }
